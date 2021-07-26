15945e6e42feefbba551d9614419a561cfb9737e
Sathish-10 <p.sathishkumar.it@gmail.com>
2021-07-26 18:23:21 +0530

    this is a second commit selenium

--- src/test/java/MavenJava/SeleniumTest.java
+++ src/test/java/MavenJava/SeleniumTest.java
@@ -8,10 +8,14 @@ public class SeleniumTest {
 	public void browserLoad()
 	{
 		System.out.println("Browser Load");
+		System.out.println("Browser Load2");
+		System.out.println("Browser Load3");
 	}
 	@Test
 	public void browserClose()
 	{
 		System.out.println("Browser Close");
+		System.out.println("Browser Close2");
+		System.out.println("Browser Close3");
 	}
 }
